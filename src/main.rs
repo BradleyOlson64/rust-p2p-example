e904d73b110318d3c63b20724209ac71e5b48ba2
BradleyOlson64 <lotrftw9@gmail.com>
2024-04-23 16:20:13 -0700

    Defining some constants

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use libp2p::{ identity, PeerId };
+use once_cell::sync::Lazy;
+use std::hash::{DefaultHasher, Hash, Hasher};
+
 fn main() {
+    let mut x = 2;
+    let &mut ref_x = &x;
+    x = 3;
     println!("Hello, world!");
 }
+
+const STORAGE_FILE_PATH: &str = "./recipes.json";
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
+
+static KEYS: Lazy<identity::Keypair> = Lazy::new(|| identity::Keypair::generate_ed25519());
+static PEER_ID: Lazy<PeerId> = Lazy::new(|| PeerId::from(KEYS.public()));
+static TOPIC: Lazy<Topic> = Lazy::new(|| Topic::new("recipes"));
